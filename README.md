85e70ee054800e4e8a51ace6a13f741c0450efa4
Marwan tamer <marwanmarwan536@gmail.com>
2022-07-09 20:16:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# udacity_project_sales_invoice_generator
+ invoce storing and creating Project for udacity java & OOP course
---
+++ src/main/java/com/mycompany/udacity_project_sales_invoice_generator/Udacity_project_sales_invoice_generator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.udacity_project_sales_invoice_generator;
+
+/**
+ *
+ * @author dell
+ */
+public class Udacity_project_sales_invoice_generator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
