38e04c209ac78167bcf29658946e1180a8d68b32
Marwan tamer <marwanmarwan536@gmail.com>
2022-07-10 15:37:49 +0200

    Creating the GUI and organizing the items.

--- src/main/java/view/MainFrame.java
+++ src/main/java/view/MainFrame.java
@@ -6,9 +6,11 @@ package view;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 /**
  *
  * @author dell
@@ -16,12 +18,13 @@ import javax.swing.*;
 public class MainFrame extends JFrame{
     
     public MainFrame(String name){
+        //creating the Home Frame.
         super(name);
-        setSize(1500,500);
         setLocation(0,0);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new GridLayout(1,2));
         
+        //Creating and adding the menu bar.
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("File");
         menuBar.add(menu);
@@ -37,8 +40,11 @@ public class MainFrame extends JFrame{
         menu.add(save);
         this.setJMenuBar(menuBar);
         
+        
+        //creating the left panel and its sub containers with mockdata.
         JPanel leftPanel=new JPanel();
-        //leftPanel.setLayout(new GridLayout(1,2));
+        leftPanel.setLayout(new BoxLayout(leftPanel,BoxLayout.Y_AXIS));
+        leftPanel.setBorder(new EmptyBorder(new Insets(10, 15, 10, 15)));
         String[][] table1Data = {
             {"1","2022-12-12","ahmed","131.21"},
             {"2","2022-12-12","ali","4214"},
@@ -46,40 +52,59 @@ public class MainFrame extends JFrame{
         };
         String[] table1Headers ={"No.","Date","Customer","Total"};
         JTable leftTable = new JTable(table1Data,table1Headers);
+        leftPanel.add(new JLabel("Invoices Table"));
         leftPanel.add(new JScrollPane(leftTable));
+        
+        JPanel leftButtonsPanel = new JPanel();
+        leftButtonsPanel.setLayout(new BoxLayout(leftButtonsPanel,BoxLayout.X_AXIS));
         JButton createBtn=new JButton("Create New Invoice");       
         JButton deleteBtn=new JButton("Delete Current Selected Invoice");   
-        leftPanel.add(createBtn);
-        leftPanel.add(deleteBtn);  
+        leftButtonsPanel.add(createBtn);
+        leftButtonsPanel.add(deleteBtn);
+        leftPanel.add(leftButtonsPanel);
         this.add(leftPanel); 
         
+        
+        //creating the right panel and its sub container with mock data.
         JPanel rightPanel=new JPanel(); 
-        rightPanel.setLayout(new GridLayout(6,2));
+        rightPanel.setLayout(new BoxLayout(rightPanel,BoxLayout.Y_AXIS));
+        leftPanel.setBorder(new EmptyBorder(new Insets(10, 15, 10, 15)));
+
+        JPanel rightInputsPanel = new JPanel();
+        rightInputsPanel.setLayout(new GridLayout(4,2));
         JTextField invoiceNumber = new JTextField();
         JTextField invoiceDate = new JTextField();
         JTextField invoiceCustomerName = new JTextField();
         JTextField invoiceTotalCost = new JTextField();
-        rightPanel.add(new JLabel("Invoice Number"));
-        rightPanel.add(invoiceNumber);
-        rightPanel.add(new JLabel("Invoice Date"));
-        rightPanel.add(invoiceDate);
-        rightPanel.add(new JLabel("Customer Name"));
-        rightPanel.add(invoiceCustomerName);
-        rightPanel.add(new JLabel("Total"));
-        rightPanel.add(invoiceTotalCost);
-                String[][] table2Data = {
+        rightInputsPanel.add(new JLabel("Invoice Number"));
+        rightInputsPanel.add(invoiceNumber);
+        rightInputsPanel.add(new JLabel("Invoice Date"));
+        rightInputsPanel.add(invoiceDate);
+        rightInputsPanel.add(new JLabel("Customer Name"));
+        rightInputsPanel.add(invoiceCustomerName);
+        rightInputsPanel.add(new JLabel("Total"));
+        rightInputsPanel.add(invoiceTotalCost);
+        rightPanel.add(rightInputsPanel);
+        
+        String[][] table2Data = {
             {"1","Ice cream","5","2","10"},
             {"2","TV","500","1","500"},
             {"1","battery","1.25","8","10"},
         };
         String[] table2Headers ={"No.","Item Name","Item Price","Item Count","Total"};
         JTable rightTable = new JTable(table2Data,table2Headers);
-        leftPanel.add(new JScrollPane(rightTable));
+        rightPanel.add(new JLabel("Invoice Items"));
+        rightPanel.add(new JScrollPane(rightTable));
+        
+        JPanel rightButtonsPanel = new JPanel();
+        rightButtonsPanel.setLayout(new BoxLayout(rightButtonsPanel,BoxLayout.X_AXIS));
         JButton saveEditBtn=new JButton("Save");       
         JButton cancelEditBtn=new JButton("Cancel");   
-        leftPanel.add(saveEditBtn);
-        leftPanel.add(cancelEditBtn);  
+        rightButtonsPanel.add(saveEditBtn);
+        rightButtonsPanel.add(cancelEditBtn);  
+        rightPanel.add(rightButtonsPanel);
+        this.add(rightPanel);
         
-        this.add(rightPanel); 
+        this.pack();
     }
 }
